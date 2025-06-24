a56074185afc2495e47b69618efee7f3dc79c7ee
Netetra <netetra07@gmail.com>
2025-06-24 14:37:52 +0900

    2025/06/24/14:37

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -97,6 +97,11 @@ export default (() => {
             return resource
           }
         })}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1546748548912571"
+          crossorigin="anonymous"
+        ></script>
       </head>
     )
   }
